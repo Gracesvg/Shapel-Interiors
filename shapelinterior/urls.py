4f6d185e442f264f0ef7172f3efbe67b63c8ffd9
Gracesvg <gracemzerah71@gmail.com>
2023-08-02 12:57:33 +0300

    added file

--- shapelinterior/urls.py
+++ shapelinterior/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.indename='shapelinteriors/index.html')
+    path('', views.index, name="index"),
+    path('about', views.about, name="about"),
+    path('blog', views.blog, name="blog"),
+    path('gallery', views.gallery, name="gallery"),
+    path('service', views.service, name="service")
+
 ]
